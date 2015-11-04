a5c4b8cd6b4a62f2dab89a3632b35c7699f0062c
Leandro Alonso <lalonso@thoughtworks.com>
2015-11-04 20:50:14 -0200

    Added forms to submit restaurant or order
    Only the HTML and CSS (SASS), already responsive

--- app/client/templates/layouts/master_layout/master_layout.js
+++ app/client/templates/layouts/master_layout/master_layout.js
@@ -2,4 +2,19 @@ Template.MasterLayout.helpers({
 });
 
 Template.MasterLayout.events({
+  'click #add-restaurant-button': () => {
+    $('#forms').css('display', 'table');
+    $('#add-restaurant').show();
+    return false;
+  },
+  
+  'click #order-something': () => {
+    $('#forms').css('display', 'table');
+    $('#add-order').show();
+    return false;
+  },
+  
+  'click .close': () => {
+    $('#forms, #add-restaurant, #add-order').hide();
+  }
 });
