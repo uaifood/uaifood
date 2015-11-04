c023d17c8ce9979259be33f0374e5e01c87ca9eb
Leandro Alonso <lalonso@thoughtworks.com>
2015-11-03 23:05:26 -0200

    Added support to ES6 and tests with Jasmine (using ES6 too)

---
+++ tests/jasmine/client/unit/tests/example.js
@@ -0,0 +1,13 @@
+describe('Test', () => {
+  'use strict';
+  
+  describe('justATestWithES6', () => {
+  
+    it('should return undefined when the book is not borrowed', () => {
+      let uaifood = true;
+      expect(uaifood).toBe(true);
+    })
+
+  }) 
+
+});
